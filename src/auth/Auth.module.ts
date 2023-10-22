286d165f89a2fdae42b2bc4df0752ad040460462
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:34:41 -0700

    chore: auth module created

---
+++ src/auth/Auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { JwtStrategy } from "./jwt.strategy";
+
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: []
+})
+
+export class AuthModule{}
