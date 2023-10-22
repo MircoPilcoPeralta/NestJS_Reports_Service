78cb50ed893ba9e3a1ba6a8ce3c9dd810b2841c3
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:32:38 -0700

    chore: user moule implemeted

--- src/User/User.module.ts
+++ src/User/User.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
+import { UserService } from "./service/User.service";
+import { UserController } from "./controller/User.controller";
 import { User } from "./schema/User.schema";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [],
-    controllers: [],
-    exports: []
+    providers: [UserService],
+    controllers: [UserController],
+    exports: [UserService]
 })
 
 export class UserModule{};
