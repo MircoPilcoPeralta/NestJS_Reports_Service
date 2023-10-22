fb780022f1d5bef122e472ec2158c2a09de3c859
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:23:41 -0700

    chore: mongoose connection implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 
 @Module({
-  imports: [],
+  imports: [    TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url:
+      'mongodb+srv://usuario:Hh8OtkYV0iptmbnN@cluster0.qzjrnxl.mongodb.net/SisDeteccionBD?retryWrites=true&w=majority',
+    entities: [join(__dirname, '**', '*.schema.{ts,js}')],
+    synchronize: true,
+    useNewUrlParser: true,
+    logging: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
