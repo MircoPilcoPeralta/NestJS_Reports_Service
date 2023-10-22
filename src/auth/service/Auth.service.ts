3de813e3a38f4e5758675dc390a785e3a6391ee5
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:43:06 -0700

    chore: login user service method implemented

--- src/auth/service/Auth.service.ts
+++ src/auth/service/Auth.service.ts
@@ -12,7 +12,22 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService) {}
 
+  async validateUser(email: string, password: string): Promise<any> {
+    const findUser = await this.userService.findByEmail(email);
 
+    if (findUser === null) {
+      return null;
+    }
+
+    const samePassword = await compare(password, findUser.password);
+
+    if (samePassword) {
+      const { password, ...result } = findUser;
+      return result;
+    }
+
+    return null;
+  }
 
   async isEmailRegistered (email: string): Promise<boolean>{
     return !!await this.userService.findByEmail(email);
@@ -23,6 +38,27 @@ export class AuthService {
     registerDto.password = hashedPassword;
     const savedUser = await this.userService.persist(registerDto);
     return this.createJWT(savedUser.id, savedUser.email);
+  }
+
+  async login(loginDto: LoginUserDto ) {
+    const {email, password } = loginDto;
+    const findUser = await this.validateUser(email, password);
+    
+    if(!findUser){
+        return null;
+    }
+
+    return this.createJWT(findUser.id, findUser.email)
+  }
+
+  async createJWT(id: string, email: string){
+    const payload: JWTPayload = { id, email };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+
+
+  async verifyToken(payload: JWTPayload) {
+    return this.userService.validateJWTPayload(payload);
   }
 
 }
