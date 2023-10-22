de5de04c8dec8b6f51077736a4889a16e9428dd0
Mirco874 <mircocuof@gmail.com>
2023-10-22 12:02:22 -0700

    chore: users module imported to reports module

--- src/Report/Report.module.ts
+++ src/Report/Report.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from "./schema/Report.schema";
 import { ReportService } from "./service/Report.service";
+import { UserModule } from "src/User/User.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Report]) ],
+    imports: [TypeOrmModule.forFeature([Report]), UserModule ],
     providers: [ReportService],
     controllers: [],
     exports: [ReportService]
