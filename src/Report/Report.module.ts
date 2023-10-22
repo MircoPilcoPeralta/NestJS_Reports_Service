25a73b978ba8590d0d271562659d4beb3f9c6775
Mirco874 <mircocuof@gmail.com>
2023-10-22 12:09:15 -0700

    feature: find report by id feature implemented

--- src/Report/Report.module.ts
+++ src/Report/Report.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from "./schema/Report.schema";
 import { ReportService } from "./service/Report.service";
+import { ReportController } from "./controller/Report.controller";
 import { UserModule } from "src/User/User.module";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Report]), UserModule ],
     providers: [ReportService],
-    controllers: [],
+    controllers: [ReportController],
     exports: [ReportService]
 })
 
--- src/Report/controller/Report.controller.ts
+++ src/Report/controller/Report.controller.ts
@@ -9,13 +9,24 @@ import { UserService } from "src/User/service/User.service";
 import { JwtAuthGuard } from "src/auth/guard/jwt-auht.guard";
 
 @Controller("api/v1/report")
-
+@UseGuards(JwtAuthGuard)
 export class ReportController {
     constructor (
         private reportService: ReportService,
         private userService: UserService
-    ){}
+        ){}
 
+    @Get(":id")
+    async findReportById( @Param("id") id: number ): Promise<HttpResponse> {
+        const findRepository = await this.reportService.findById(id);
+
+        if(!findRepository){
+            throw new NotFoundException(
+                new HttpResponse(404, "Reporte no encontrado")
+            );
+        }
+        return new HttpResponse(200,"Reporte encontrado correctamente", this.reportEntityToVo(findRepository));
+    }
 
     @Post()
     async persistReport(@Body() dto: CreateReportDto): Promise<HttpResponse> {
@@ -26,6 +37,7 @@ export class ReportController {
         const savedReport = await this.reportService.persistReport(dto);
         return new HttpResponse(201, "Reporte guardado correctamente", this.reportEntityToVo(savedReport));        
     }
+
 
 
     private reportEntityToVo(report: Report): ReportVo{
