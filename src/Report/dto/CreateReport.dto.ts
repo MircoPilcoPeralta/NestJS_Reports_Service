4dbbe187a6294af54b89f18fa697be94efca2c0f
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:54:58 -0700

    chore: create report dto added

---
+++ src/Report/dto/CreateReport.dto.ts
@@ -0,0 +1,9 @@
+export class CreateReportDto{
+    cameraName: string;
+    inference: number;
+    message: string;
+    date: string;
+    time: string;
+    imageURL: string;
+    userId: string;
+}
