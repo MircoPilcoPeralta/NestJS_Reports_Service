bbd3344af926e019a42ad86e54029f881dddcac3
Mirco874 <mircocuof@gmail.com>
2023-10-22 11:38:21 -0700

    chore: register user service method implemented

---
+++ src/auth/service/Auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/User/service/User.service';
+import { RegisterUserDto } from 'src/User/dto/RegisterUser.dto';
+import { hash, compare } from 'bcrypt';
+import { JWTPayload } from '../JWTPayload.model';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService) {}
+
+
+  async register(registerDto: RegisterUserDto) {
+    const hashedPassword = await hash(registerDto.password, 10);
+    registerDto.password = hashedPassword;
+    const savedUser = await this.userService.persist(registerDto);
+    return this.createJWT(savedUser.id, savedUser.email);
+  }
+
+}
